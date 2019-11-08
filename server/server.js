0b35e32b36160f41645bab9fa6ff39d190cc2c56
Josefin Nilsson <josefin.l.nilsson@gmail.com>
2019-11-08 14:15:48 +0100

    Add possibility to configure delayed response

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 const express = require('express')
-const app = express()
 const dostroy = require('dostroy')
+const sleep = require('sleep')
+
+const app = express()
+
+const slow = true
 
 app.use(dostroy.countRequests)
 
@@ -8,5 +12,6 @@ app.listen(3000)
 
 app.get('/', function (req, res) {
   console.log('Number of requests: ' + dostroy.getRequests())
-  res.send('Hello from example server!')
+  slow && sleep.msleep(500)
+  res.send('Hello from example server! \n')
 })
